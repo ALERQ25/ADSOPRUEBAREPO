e1755b693fef5b773249e16a7402ea7c293e066b
Alejandro Ramìrez <quiscue22@gmail.com>
2024-11-29 19:54:57 -0500

    Segunda rama

--- Codigo/ConsoleApp1/Program.cs
+++ Codigo/ConsoleApp1/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El número es par.");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar.");
+            }
+            Console.ReadKey();
+
         }
     }
 }
